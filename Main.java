eafff5426c22ee627a3b954b5dd9bdb5df121eb6
samuele <samu.diani@gmail.com>
2024-02-20 11:23:36 +0100

    primo salvataggio

---
+++ Main.java
@@ -0,0 +1,27 @@
+package Sudoku;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        System.out.println("Cosa vuoi fare? 1.Inserisci numero. 2.Conntrolla errori. 3.Visualizza griglia.");
+        int scelta = in.nextInt();
+
+        //inserire numero
+        if (scelta == 1) {
+            //codice supermegafighissimo
+        }
+
+        //controlla errori
+        else if (scelta == 2) {
+
+        }
+
+        //visualizza griglia
+        else {
+
+        }
+    }
+}
